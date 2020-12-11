6e06b3bbc0a8d27f055f61bf6612b6360ce93433
Robin Wieruch <hello@rwieruch.com>
2020-12-11 14:43:55 +0100

    foo

---
+++ README.md
@@ -0,0 +1,11 @@
+# react-usereducer-middleware
+
+Showcasing how middleware works with React's useReducer Hook. [Read more about it.](https://www.robinwieruch.de/react-usereducer-middleware/)
+
+## Installation
+
+- `git clone git@github.com:the-road-to-learn-react/react-usereducer-middleware.git`
+- cd react-usereducer-middleware
+- npm install
+- npm start
+- visit `http://localhost:3000`
---
+++ src/App.js
@@ -0,0 +1,111 @@
+import * as React from 'react';
+
+const initialTodos = [
+  {
+    id: 'a',
+    task: 'Learn React',
+    complete: false,
+  },
+  {
+    id: 'b',
+    task: 'Learn Firebase',
+    complete: false,
+  },
+];
+
+const todoReducer = (state, action) => {
+  switch (action.type) {
+    case 'DO_TODO':
+      return state.map((todo) => {
+        if (todo.id === action.id) {
+          return { ...todo, complete: true };
+        } else {
+          return todo;
+        }
+      });
+    case 'UNDO_TODO':
+      return state.map((todo) => {
+        if (todo.id === action.id) {
+          return { ...todo, complete: false };
+        } else {
+          return todo;
+        }
+      });
+    default:
+      return state;
+  }
+};
+
+const loggerBefore = (action, state) => {
+  console.log('logger before:', action, state);
+};
+
+const loggerAfter = (action, state) => {
+  console.log('logger after:', action, state);
+};
+
+const useReducerWithMiddleware = (
+  reducer,
+  initialState,
+  middlewareFns,
+  afterwareFns
+) => {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+
+  const aRef = React.useRef();
+
+  const dispatchWithMiddleware = (action) => {
+    middlewareFns.forEach((middlewareFn) =>
+      middlewareFn(action, state)
+    );
+
+    aRef.current = action;
+
+    dispatch(action);
+  };
+
+  React.useEffect(() => {
+    if (!aRef.current) return;
+
+    afterwareFns.forEach((afterwareFn) =>
+      afterwareFn(aRef.current, state)
+    );
+  }, [afterwareFns, state]);
+
+  return [state, dispatchWithMiddleware];
+};
+
+const App = () => {
+  const [todos, dispatch] = useReducerWithMiddleware(
+    todoReducer,
+    initialTodos,
+    [loggerBefore],
+    [loggerAfter]
+  );
+
+  const handleChange = (todo) => {
+    dispatch({
+      type: todo.complete ? 'UNDO_TODO' : 'DO_TODO',
+      id: todo.id,
+    });
+  };
+
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          <label>
+            <input
+              type="checkbox"
+              checked={todo.complete}
+              onChange={() => handleChange(todo)}
+            />
+            {todo.task}
+          </label>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default App;
